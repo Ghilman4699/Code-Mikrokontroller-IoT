d7a581d3e2a0150ee3a0c6357412b8c324c82b7f
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-04-08 22:49:51 +0700

    Update BareMinimumESP.ino

--- BareMinimumESP.ino
+++ BareMinimumESP.ino
@@ -1,1 +1,43 @@
 #include <Arduino.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include "parse_merge.h"
+
+void WiFi_Connect(String data_ssid, String data_pwd){
+  bool stat = false;
+  int elapsed_time = 0;
+  
+  Serial.println("[*] Connecting..");
+  elapsed_time = millis();
+  WiFi.begin(data_ssid.c_str(), data_pwd.c_str());
+  Serial.print('[');
+  while(WiFi.status() != WL_CONNECTED){
+    if( ((elapsed_time/1000) - (millis()/1000)) > 10 ){
+      stat = false;
+      break;
+    }
+    
+    Serial.print('*');
+    stat = true;
+    delay(150);
+  }
+  Serial.print(']');
+  
+  if(stat)
+    Serial.println("[+] Connected!");
+  else
+    Serial.println("[-] Error while connecting to WiFi!");
+}
+
+void setup(){
+  Serial.begin(115200);
+  WiFi_Connect("myWiFiName", "myWiFiPwd");
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    data = Serial.readStringUntil('\n');
+    parse_string();
+  }
+  
+}
