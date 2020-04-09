0a6980d78fb74a813dd92c4599b1ef90060fd076
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-04-09 22:47:25 +0700

    Create Coba_Sender.ino

---
+++ Coba_Sender.ino
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include "parse_merge.h" // <-- important library
+
+// Global variable
+static String data_array[6] = {};
+
+// Function declaration
+void send_command(String cmd, unsigned char index){
+  data_array[index] = cmd;
+  String new_data = "";
+  
+  for(int x=0;x<6;x++)
+    new_data += (data_array[x] + ";");
+  Serial.println(new_data);
+}
+
+void setup(){
+  Serial.begin(115200);
+
+  // Fill empty array
+  for(int x=0;x<6;x++)
+    data_array[x] = "000";
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    data = Serial.readStringUntil('\n');
+    parse_string();
+  }
+  
+  send_command("200", 0);
+  send_command("tes", 1);
+  send_command("oke", 2);
+  send_command("done", 3);
+  send_command("50.5", 4);
+  send_command("34f", 5);
+}
