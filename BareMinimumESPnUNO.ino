e639f926c86846be8266a39057d886897ab7ea01
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-04-09 22:30:50 +0700

    Update BareMinimumESPnUNO.ino

--- BareMinimumESPnUNO.ino
+++ BareMinimumESPnUNO.ino
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 #include "parse_merge.h" // <-- important library
 
+// Global variable
+static String data_array[5] = {};
+
+// Function declaration
+void send_command(String cmd, unsigned char index){
+  data_array[index] = cmd;
+  String new_data = "";
+  
+  for(int x=0;x<5;x++)
+    new_data += (data_array[x] + ";");
+  Serial.println(new_data);
+}
+
 void setup(){
   Serial.begin(115200); // <-- enlarge the data transmission size
 
+  // Fill empty array
+  for(int x=0;x<5;x++)
+    data_array[x] = "000";
+  
   // Once run code put here
 }
 
